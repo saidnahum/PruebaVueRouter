2c20c6416c156eca9430962d5905e834b68e9766
saidnahum <70162272+saidnahum@users.noreply.github.com>
2021-06-23 23:24:17 -0500

    update

--- src/views/Articulo.vue
+++ src/views/Articulo.vue
@@ -1,10 +1,14 @@
 <template>
-    Articulo
+    <Titulo texto="Ruta con parámetros" />
+    <h2>Parámetro URL: {{ $route.params.id }}</h2>
 </template>
 
 <script>
+import Titulo from '../components/Titulo.vue'
 export default {
-
+    components: {
+        Titulo
+    }
 }
 </script>
 
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,9 @@
 <template>
     <Titulo texto="Titulo del Blog" />
+    <!-- <button @click="consumirApi">Consumir API</button> -->
+    <div v-for="item in arrayBlog" :key="item.id">
+        {{ item.title }}
+    </div>
 </template>
 
 <script>
@@ -7,6 +11,26 @@ import Titulo from '../components/Titulo.vue'
 export default {
     components: {
         Titulo
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {
+        async consumirApi(){
+            try {
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+                const array = await data.json()
+                console.log(array)
+                this.arrayBlog = array
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },
+    created(){
+        this.consumirApi()
     }
 }
 </script>
